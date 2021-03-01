8412ede288a9f8ed28b1a74ae10f5a4493135117
Lucas Chicoski <lucaschicoski@directy.com.br>
2021-03-01 15:08:20 -0300

    CÓDIGO DOCUMENTADO

--- EnvioEmail.js
+++ EnvioEmail.js
@@ -1,23 +1,37 @@
+/**
+ * Empresa: Directy
+ * Autor: Lucas Chicoski dos Santos
+ * 
+ * Sistema de envio de email para empresa Disbat. O sistema envia emails periodicamente para as tres
+ * empresas cadastradas, Disbat, Polo, Norte sul.
+ */
+
 const sql = require("msnodesqlv8");
 const data = require('date-fns');
 var nodemailer = require('nodemailer');
 var sleep = require('system-sleep');
 const { ro } = require("date-fns/locale");
 
+//conexão com o banco de dados
 const connectionString = "server=Servidor2;Database=disbat_automate;Trusted_Connection=Yes;Driver={SQL Server Native Client 11.0}";
 
+//Query de pesquisa
 const query1 = "select cliente, empresa, email from notifica_email where email is not null group by cliente, empresa, email";
 //select cliente, empresa, email from notifica_email where email is not null group by cliente, empresa, email
 //select cliente, empresa, email from notifica_email where empresa = 'POLO' and email is not null group by cliente, empresa, email
 //select cliente, empresa, email from notifica_email where empresa = 'NORTESUL' and email is not null group by cliente, empresa, email
 
+//=============================================================================================================================================
 
+//Realizando a conexão passando como parâmetro a consulta e a coexão com o banco
 sql.query(connectionString, query1, (err1, rows) => {
 
     var clienteJS = [rows];
   
     tamanho = Object.keys(clienteJS[0]).length;
     
+    //1° for - retorna todas as linhas que contém no banco de dados trazendo as informações
+    //cliente, email, empresa
     for (let j = 0; j < tamanho ; j++) {
 
         sleep(2000)
@@ -25,6 +39,12 @@ sql.query(connectionString, query1, (err1, rows) => {
         var cliente = rows[j].cliente;
         var empresa = rows[j].empresa;
         var emailEmpresa = "";
+
+        //=========================SELETOR DE EMAIL=====================
+
+        /**
+         * ESSA ETAPA SELECIONA QUAL EMAIL QUE IRÁ ENVIAR PARA CADA CLIENTE
+         */
 
         if (empresa === "DISBAT") {
 
@@ -69,10 +89,13 @@ sql.query(connectionString, query1, (err1, rows) => {
                 }
             });
         }
-       
+       //=========================FIM SELETOR DE EMAIL=====================
+
+
+       // ESTA CONSULTA RETORNA APENAS OS EMAILS DOS CLIENTES
         var query = `select * from notifica_email where cliente like '${cliente}'`;
 
-        
+        //Realizando a conexão passando como parâmetro a consulta e a coexão com o banco
         sql.query(connectionString, query, (err2, rows2) => {
 
             
@@ -91,8 +114,13 @@ sql.query(connectionString, query1, (err1, rows) => {
             var clienteS = [];
             var empresaS = [];
             var emaiL = [];
-
+            
             for (var i = 0; i < tamanho2; i++) {
+
+                /**
+                 * ESTE FOR MONTA A MENSAGEM QUE SERÁ ENVIADA PARA O
+                 * CLIENTE
+                 */
 
                 arrayDt[i] = rows2[i].dt_emissao;
                 Data = data.format(new Date(arrayDt[i]), "dd-MM-yyyy")
@@ -104,7 +132,6 @@ sql.query(connectionString, query1, (err1, rows) => {
                 somatorioSucata = somatorioSucata + rows2[i].sucata;
 
                 cabecalho = `Caro ${cliente},<br> A ${empresa} informa que há as seguintes quantidades de sucata em aberto:<br><br>`
-
                 
                 mensagem = mensagem + `<tr><td>${Data}</td><td>${arrayNumDock[i]}</td><td>${arraySucata[i]}</td></tr>`
                 rodape = `\n\nTotal em aberto: ${somatorioSucata} KG\n\n`
@@ -113,6 +140,7 @@ sql.query(connectionString, query1, (err1, rows) => {
             }
            
 
+            //MONTA AS INFORMAÇÕES DO DESTINATÁRIO
             var destinatario = {
                 from: emailEmpresa, //de quem
                 to: emaiL , //emaiL
@@ -130,13 +158,16 @@ sql.query(connectionString, query1, (err1, rows) => {
             };
 
             async function enviar() {
+                /**
+                 * FUNÇÃO QUE ENVIA OS EMAILS
+                 */
                 await remetente.sendMail(destinatario, (err, inf) =>{
                     console.log(err);
                 });
             }
             
         
-            enviar();
+            enviar(); //INSTANCIA A FUNÇÃO
             console.log("Email enviado para " + cliente);
         })
 
@@ -144,6 +175,12 @@ sql.query(connectionString, query1, (err1, rows) => {
     }//fim do for
 
 })
+
+/**
+ * CAOS TODAS OS EMAILS FOREM ENVIADOS, 
+ * SERÁ DISPARADO UM EMAIL NOTIFICANDO 
+ * QUE TODOS OS EMAILS FORAM ENVIADOS.
+ */
 
 var remetente = nodemailer.createTransport({
     service: 'outlook', //serviço
